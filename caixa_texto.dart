82afb8db238aea0bd71367bf9196fa4a2651ea80
EricKSams <116317823+EricKSams@users.noreply.github.com>
2023-05-19 08:51:47 -0300

    Update caixa_texto.dart

--- caixa_texto.dart
+++ caixa_texto.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
 
 class CaixaTexto extends StatelessWidget {
-  final label;
-  final pass;
-  const CaixaTexto({super.key, required this.label, this.pass = false});
+  final String label;
+  final bool pass;
+  final TextEditingController? controller;
+  const CaixaTexto(
+      {super.key,
+      required this.label,
+      this.pass = false,
+      required this.controller});
 
-  const CaixaTexto.senha({super.key, required this.label, this.pass = true});
+  const CaixaTexto.senha(
+      {super.key,
+      required this.label,
+      this.pass = true,
+      required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(50.0),
-      child: TextFormField(
-        validator: (value) {
-          if (value!.isEmpty) {
-            return 'por favor, preencha este campo';
-          }
-        },
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
         obscureText: pass,
+        controller: controller,
         decoration: InputDecoration(
-            labelText: label,
-            border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(2000))),
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
       ),
     );
   }
